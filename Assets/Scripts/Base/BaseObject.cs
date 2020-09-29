726208f3d7959ea7ed89049194fd4190d8478da2
Ali Nazif Koca <onplayerexitvehicle@outlook.com>
2020-09-29 22:25:42 +0300

    Platforms and base scripts implemented

---
+++ Assets/Scripts/Base/BaseObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Base
+{
+    public abstract class BaseObject : MonoBehaviour
+    {
+        public virtual void ObjectAwake(){}
+        
+        public virtual void ObjectStart(){}
+        
+        public virtual void ObjectUpdate(){}
+        
+        public virtual void ObjectFixedUpdate(){}
+        
+        public virtual void ObjectLateUpdate(){}
+    }
+}
---
+++ Assets/Scripts/Management/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Base;
+using UnityEngine;
+
+namespace Management
+{
+    [DisallowMultipleComponent]
+    public class GameManager: MonoBehaviour
+    {
+        [SerializeField] private List<BaseObject> baseObjects = new List<BaseObject>();
+
+        private void Awake()
+        {
+            for (int i = 0; i < baseObjects.Count; i++)
+            {
+                baseObjects[i].ObjectAwake();
+            }
+        }
+
+        private void Start()
+        {
+            for (int i = 0; i < baseObjects.Count; i++)
+            {
+                baseObjects[i].ObjectStart();
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < baseObjects.Count; i++)
+            {
+                baseObjects[i].ObjectUpdate();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            for (int i = 0; i < baseObjects.Count; i++)
+            {
+                baseObjects[i].ObjectFixedUpdate();
+            }
+        }
+
+        private void LateUpdate()
+        {
+            for (int i = 0; i < baseObjects.Count; i++)
+            {
+                baseObjects[i].ObjectLateUpdate();
+            }
+        }
+    }
+}
