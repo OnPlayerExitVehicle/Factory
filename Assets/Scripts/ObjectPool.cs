62ffe38b206fa5e3a1855585a8b156627a765cbe
Ali Nazif Koca <onplayerexitvehicle@outlook.com>
2020-10-01 16:11:20 +0300

    feat: Upgraded Saw System

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -6,18 +6,20 @@ using Random = UnityEngine.Random;
 
 public class ObjectPool: MonoBehaviour
 {
-    private List<Stack<GameObject>> pool = new List<Stack<GameObject>>();
+    public List<Stack<GameObject>> woodPool = new List<Stack<GameObject>>();
+    public List<Stack<GameObject>> logPool = new List<Stack<GameObject>>();
     
     private readonly int layerOffset = 8;
-    private readonly int maxObjects = 5;
-
-    private Random random = new Random();
+    private readonly int maxWood = 5;
+    private readonly int maxLog = 20;
+    private readonly Quaternion defaultRotation = Quaternion.Euler(0, 0, 90);
 
     public ObjectPool()
     {
         for (int i = 0; i < 2; i++)
         {
-            pool.Add(new Stack<GameObject>());
+            woodPool.Add(new Stack<GameObject>());
+            logPool.Add(new Stack<GameObject>());
         }
         
     }
@@ -25,35 +27,79 @@ public class ObjectPool: MonoBehaviour
     {
         GameObject wood;
         int typeNumber = (int) type;
-        Debug.Log($"{typeNumber}.inci stack'te {pool[typeNumber].Count} obje var");
-        if (pool[typeNumber].Count > 1)
+        
+        Debug.Log($"{typeNumber}.inci stack'te {woodPool[typeNumber].Count} obje var");
+        if (woodPool[typeNumber].Count > 1)
         {
-            wood = pool[typeNumber].Pop();
+            wood = woodPool[typeNumber].Pop();
             wood.SetActive(true);
+            
+            wood.transform.rotation = defaultRotation;
+            Rigidbody rigidbody = wood.GetComponent<Rigidbody>();
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.angularVelocity = Vector3.zero;
+            
             return wood;
         }
         else
         {
-            GameObject instanceWood = pool[typeNumber].Pop();
+            GameObject instanceWood = woodPool[typeNumber].Pop();
             wood = Instantiate(instanceWood);
             PushWood(instanceWood);
+            
             wood.SetActive(true);
             return wood;
         }
         
     }
 
-    public void PushWood(GameObject gameObject)
+    public GameObject PopLog(WoodType type)
     {
-        if (pool[gameObject.layer - layerOffset].Count < maxObjects)
+        Debug.Log("LogPOPPED");
+        GameObject log;
+        int typeNumber = (int) type;
+        
+        if (logPool[typeNumber].Count > 1)
         {
-            gameObject.SetActive(false);
-            pool[gameObject.layer - layerOffset].Push(gameObject);
+            log = logPool[typeNumber].Pop();
+            log.SetActive(true);
+            return log;
         }
         else
         {
-            Destroy(gameObject);
+            GameObject instanceLog = logPool[typeNumber].Pop();
+            log = Instantiate(instanceLog);
+            PushLog(instanceLog);
+            
+            log.SetActive(true);
+            return log;
         }
-        Debug.Log($"{gameObject.layer - layerOffset}.inci stack'e obje eklendi. Artik {pool[gameObject.layer - layerOffset].Count} obje var");
+    }
+
+    public void PushWood(GameObject wood)
+    {
+        if (woodPool[wood.layer - layerOffset].Count < maxWood)
+        {
+            wood.SetActive(false);
+            woodPool[wood.layer - layerOffset].Push(wood);
+        }
+        else
+        {
+            Destroy(wood);
+        }
+        Debug.Log($"{wood.layer - layerOffset}.inci stack'e obje eklendi. Artik {woodPool[wood.layer - layerOffset].Count} obje var");
+    }
+
+    public void PushLog(GameObject log)
+    {
+        if (woodPool[log.layer - layerOffset].Count < maxLog)
+        {
+            log.SetActive(false);
+            logPool[log.layer - layerOffset].Push(log);
+        }
+        else
+        {
+            Destroy(log);
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,30 +7,41 @@ using Extensions;
 using Random = System.Random;
                                                                                                                         // Spawner Length = 3.5f
                                                                                                                         // Spawner Width = 1.5f
+[RequireComponent(typeof(ObjectPool))]
 public class Spawner : BaseObject
 {
-    private const float SpawnerLength = 3.5f;
+    private const float SpawnerLength = 3f;
     private const float SpawnerWidth = 1.6f;
 
     // Serialized Fields
     [SerializeField] private List<GameObject> woodPrefabList;
+    [SerializeField] private List<GameObject> logPrefabList;
     [SerializeField] private Vector3 spawnPosition;
     [SerializeField] private float spawnTime;
+    [SerializeField] private float minWoodRound = 0.25f;
+    [SerializeField] private float maxWoodRound = 1f;
+    [SerializeField] private float minWoodLength = 0.5f;
+    [SerializeField] private float maxWoodLength = 1f;
     
-    //Serialized Collections
-    [SerializeField] private List<Material> materialList;
-    [SerializeField] private List<Vector3> scaleList;
+    // Serialized Collections
+    [SerializeField] private List<Vector3> scaleList; //
     
     
     // Default Fields
     private readonly Random random = new Random();
-    private ObjectPool objectPool = new ObjectPool();
+    private ObjectPool objectPool;
 
     public override void ObjectAwake()
     {
+        objectPool = GetComponent<ObjectPool>();
         foreach (GameObject woodPrefab in woodPrefabList)
         {
             objectPool.PushWood(woodPrefab);
+        }
+
+        foreach (GameObject logPrefab in logPrefabList)
+        {
+            objectPool.PushLog(logPrefab);
         }
         
         spawnPosition = transform.position; //
@@ -54,26 +65,25 @@ public class Spawner : BaseObject
 
     private void SpawnRandomWood()
     {
-        GameObject wood = objectPool.PopWood((WoodType) random.Next(0, 1));
+        GameObject wood = objectPool.PopWood((WoodType) random.Next(0, 2));
         wood.transform.position = spawnPosition;
-        wood.transform.localScale = scaleList.Random();
-        //wood.transform.position = RandomSpawnPosition(transform.localScale);
-        Debug.Log(transform.position);
-        wood.GetComponent<Renderer>().material = materialList.Random();
+        Vector3 scale = Vector3.one;
+        wood.transform.localScale = scale;
+        wood.transform.position = new Vector3(0, 11, 3.05f); //+ (Vector3.right * UnityEngine.Random.Range(-0.1f, 0.1f));
     }
 
-    
-    /*public Vector3 RandomSpawnPosition(Vector3 scale)
+    private Vector3 RandomScale()
     {
-        float horizontalSpace = SpawnerLength - scale.y;
-        float verticalSpace = SpawnerWidth - scale.x;
-        float randomPositionX = transform.position.x + random.Next(-verticalSpace, verticalSpace);
-        float randomPositionZ = transform.position.z + random.Next(-horizontalSpace, horizontalSpace);
-        
-        return new Vector3(randomPositionX, transform.position.y, randomPositionZ);
+        float round = UnityEngine.Random.Range(minWoodRound, maxWoodRound);
+        return new Vector3(round, UnityEngine.Random.Range(minWoodLength, maxWoodLength), round);
     }
+    
+    public Vector3 RandomSpawnPosition(Vector3 scale)
+    {
+        float horizontalSpace = SpawnerLength - scale.y * 2;
+        float verticalSpace = SpawnerWidth - scale.x;
+        float randomPositionX = transform.position.x + UnityEngine.Random.Range(-verticalSpace, verticalSpace);
 
-    private Transform SetRandomTransform(){}
-    */
-  
+        return new Vector3(randomPositionX, transform.position.y, transform.position.z);
+    }
 }
